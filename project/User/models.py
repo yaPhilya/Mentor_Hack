379a2307162fb0b15727d969b3295c9f7efedbe3
NifantovaIrina <nifantova98@gmail.com>
2018-02-13 12:58:54 +0300

    It doesn't work!

--- project/User/models.py
+++ project/User/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User as BaseUser
 # Create your models here.
+
+class User(BaseUser):
+    location = models.CharField(max_length=30)
+    
